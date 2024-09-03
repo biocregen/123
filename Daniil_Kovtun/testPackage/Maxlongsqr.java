b0df2106b764e0dbf92462f6d3a5ce072da0bd2e
lodjers <danyakovtun2012@mail.ru>
2024-09-03 21:07:10 +0300

    test

---
+++ Daniil_Kovtun/testPackage/Maxlongsqr.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import java.math.BigInteger;
+
+public class Maxlongsqr {
+    public static BigInteger maxLongSqr() {
+        BigInteger a = BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger b = BigInteger.valueOf(Long.MAX_VALUE);
+        a = a.multiply(b);
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println(maxLongSqr());
+    }
+}
