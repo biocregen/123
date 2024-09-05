01d78daeb7a1db56080a9fb36db23990a24f6d43
lodjers <danyakovtun2012@mail.ru>
2024-09-05 21:00:00 +0300

    test

---
+++ Daniil_Kovtun/testPackage/FactorialRecursion.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FactorialRecursion {
+    public static BigInteger factorialRecursion(int value) {
+        if (value == 0 || value == 1) {
+            return BigInteger.valueOf(1);
+        } else {
+                return BigInteger.valueOf(value).multiply(factorialRecursion(value-1));
+            }
+        }
+    public static void main(String[] args) {
+        System.out.println("Введите переменную: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        System.out.println(factorialRecursion(value));
+    }
+}
