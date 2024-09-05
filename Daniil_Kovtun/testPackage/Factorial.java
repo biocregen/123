b03004eeda0b878affb226adaa6e208c1d52550c
lodjers <danyakovtun2012@mail.ru>
2024-09-05 20:07:08 +0300

    test

---
+++ Daniil_Kovtun/testPackage/Factorial.java
@@ -0,0 +1,23 @@
+package testPackage;
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Factorial {
+        public static BigInteger factorial(int value) {
+            BigInteger result = BigInteger.valueOf(1);
+            if (value == 0) {
+                result = BigInteger.valueOf(1);
+            } else {
+                for (int i = 1; i <= value; i++ ) {
+                    result = result.multiply(BigInteger.valueOf(i));
+                }
+            }
+        return result;
+        }
+        public static void main(String[] args) {
+            System.out.println("Введите переменную: ");
+            Scanner scanner = new Scanner(System.in);
+            int value = scanner.nextInt();
+            System.out.println(factorial(value));
+        }
+}
