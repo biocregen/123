9d8e15011e4940618ce805097457475cad6ce38b
lodjers <danyakovtun2012@mail.ru>
2024-09-05 23:32:45 +0300

    определяется день недели с использованием тернарного оператора

---
+++ Daniil_Kovtun/testPackage/IsWeekendTernarnii.java
@@ -0,0 +1,17 @@
+package testPackage;
+
+import java.util.Scanner;
+
+public class IsWeekendTernarnii {
+    public static String isWeekendtern (String weekday) {
+        return ((weekday.equals("Sunday")) || (weekday.equals("Saturday"))) ? "Ура, выходной!" : "Надо еще поработать";
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите день:");
+        String weekend = scanner.nextLine();
+        //String weekend = "Sunday";
+        System.out.println(isWeekendtern(weekend));
+
+    }
+}
