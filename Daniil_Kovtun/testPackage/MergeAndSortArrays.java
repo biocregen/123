6f75454c4760b59c509c1abd61b8286361de8faf
lodjers <danyakovtun2012@mail.ru>
2024-09-07 17:40:51 +0300

    соединяем два массива и сортируем полученный массив; с использованием Arrays

---
+++ Daniil_Kovtun/testPackage/MergeAndSortArrays.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+import java.util.Arrays;
+
+public class MergeAndSortArrays {
+    public static int[] mergeAndSortArrays (int[] firstArray, int[] secondArray) {
+        int[] r = new int[firstArray.length + secondArray.length];
+        System.arraycopy(firstArray, 0, r, 0, firstArray.length);
+        System.arraycopy(secondArray, 0, r, firstArray.length, secondArray.length);
+        Arrays.sort(r);
+        return r;
+    }
+}
