760a64a159c59e752c47054cfb0250afcfbfe413
lodjers <danyakovtun2012@mail.ru>
2024-09-07 13:17:10 +0300

    вывод введенного массива

---
+++ Daniil_Kovtun/testPackage/PrintArray.java
@@ -0,0 +1,18 @@
+package testPackage;
+
+public class PrintArray {
+    public static void printArray(int[] numbers) {
+        System.out.print("[");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+    public static void main(String[] args) {
+        int [] nums = {1, 2, 3, 4, 5};
+        printArray(nums);
+    }
+}
