77d50b054d2459916b92bc7eec633dd8cc49180d
lodjers <danyakovtun2012@mail.ru>
2024-09-19 00:41:05 +0300

    задача с tryWithResourses

---
+++ Daniil_Kovtun/Exceptions/CreditForCliend/car/carTryWithResourses.java
@@ -0,0 +1,19 @@
+package Exceptions.CreditForCliend.car;
+
+public class carTryWithResourses {
+    public static class Car implements AutoCloseable{
+        public void close() {
+            System.out.println("Машина закрывается...");
+        }
+        public void drive() {
+            System.out.println("Машина поехала");
+        }
+    }
+
+    public static void main(String[] args) {
+        try(Car car = new Car()) {
+            car.drive();
+        } catch (Throwable a) {}
+    }
+}
+
