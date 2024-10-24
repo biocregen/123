a933d353d290fe3d8d9e490a1f172983a57f2a97
lodjers <danyakovtun2012@mail.ru>
2024-10-24 21:03:51 +0300

    задача с min max в Stream

---
+++ Daniil_Kovtun/Module6/TaskMinMaxStream.java
@@ -0,0 +1,28 @@
+package Module6;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+    public class TaskMinMaxStream {
+        public static <T> void findMinMax(Stream <? extends T> stream, Comparator<? super T> order,
+                                          BiConsumer<? super T, ? super T> minMaxConsumer) {
+            List<? extends T> list = stream.sorted(order).collect(Collectors.toList());
+            if (list.isEmpty()) {
+                minMaxConsumer.accept(null, null);
+            } else {
+                minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+            }
+        }
+
+        public static void main(String[] args) {
+            Stream <Integer> stream = Stream.of(123, 54325, 456, 567, 768);
+            Comparator <? super Integer> comparator = Integer::compare;
+            BiConsumer<? super Integer, ? super Integer> biConsumer =
+                    (x1, x2) -> System.out.println("min: " + x1 + "\nmax: " + x2);
+            findMinMax(stream, comparator, biConsumer);
+        }
+    }
+
+
